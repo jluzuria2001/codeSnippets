758953a33faf72c22fb3e459fd4ab675e914fa58
jluzuria2001 <jluzuria2001@users.noreply.github.com>
2015-03-12 10:43:39 +0100

    Create Variable2Factor.r

---
+++ Variable2Factor.r
@@ -0,0 +1,19 @@
+#sample data
+myData <- data.frame(A=rep(1:2, 3), B=rep(1:3, 2), Pulse=20:25)
+
+#convert variables to factors
+myData$A <- as.factor(myData$A)
+myData$B <- as.factor(myData$B)
+
+#see our data
+myData
+
+#calcs over out data
+mean(myData$A, na.rm=TRUE)
+
+#see our levels
+myData$B
+
+#define levels with a specific name
+levels(myData$A) <- c("long", "short")
+levels(myData$B)<-c("1Kg","2Kg","3Kg")
