252c7721a2df2600172211af7540342a172c53a6
jluzuria2001 <jluzuria2001@users.noreply.github.com>
2015-06-18 13:17:42 +0200

    Create time.py

---
+++ time.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import sys
+import time
+
+def timestamp():
+   now = time.time()
+   localtime = time.localtime(now)
+   milliseconds = '%03d' % int((now - int(now)) * 1000)
+   return time.strftime('%Y%m%d%H%M%S', localtime) + milliseconds
+
+value=timestamp()
+
+print value
+
