a68be06eb56fec176ff661e0b2fc5e72611bbf63
jluzuria2001 <jluzuria2001@users.noreply.github.com>
2016-10-28 12:55:42 +0200

    Update embeddedFonts.sh

--- embeddedFonts.sh
+++ embeddedFonts.sh
@@ -1,3 +1,7 @@
+# Embed a font into a PDF 
+# with free linux command line tools
+# 
+
 # Preflight Report
 # Errors were found on pages 4-6
 # Font Helvetica, Helvetica-Bold, ZapfDingbats is not embedded (1343x on pages 4-6)
@@ -10,3 +14,10 @@ $ gs -q -dNOPAUSE -dBATCH -dPDFSETTINGS=/prepress -sDEVICE=pdfwrite -sOutputFile
 
 # All the fonts are embedded
 $ pdffonts output.pdf
+
+####################################
+# Other solution seems to be to do 
+# PDF->PS->PDF conversion
+
+$ pdf2ps file.pdf file.ps
+$ ps2pdf14 -dPDFSETTINGS=/prepress -dEmbedAllFonts=true file.ps file.pdf
